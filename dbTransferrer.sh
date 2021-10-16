60059b6508f16ce4ecfc170992a76e99ccf2353b
hippocritical <41228167+hippocritical@users.noreply.github.com>
2021-10-16 10:58:25 +0200

    added dbtransferrer
    script that automatically transfers specific files to your pc whenever you execute it.
    Currently using it to automatically copy all weight history files and sqlite files onto my machine to then let it be read by grafana and show it.

---
+++ dbTransferrer.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#this bash script is used to transfer files from multiple VPSes to your home pc to have it show up in a grafana database (or anything else you please).
+#you could crontab -e the following command to let it run every hour:
+#0 * * * * [path-to-location]/[filename.sh]
+ssh-add ~/freqtrade/oracle.key #edit it to your liking
+key_loc="/home/hippocrite/freqtrade/oracle.key" #not necessary if you did ssh-add but can never hurt.
+fileCopier()
+{
+    USER=$1
+    IP=$2
+    SourceDir=$3
+    TargetDir="/home/hippocrite/Grafana/DBs/" #edit it to your liking
+    FileName=$4
+    
+    SourceFullPath=$SourceDir$FileName
+    TargetFullPath=$TargetDir$FileName
+    
+    scp -i $key_loc $USER@$IP:$SourceFullPath $TargetFullPath
+}
+
+#example for a csv file to be transferred to your pc from a VPS
+#fileCopier ubuntu [VPS_IP] /home/ubuntu/ weightwatch_arm1.txt
+
+#example for a sqlite file to be transferred to your pc from a VPS
+#fileCopier ubuntu [VPS_IP] trades_NFI_USDT.sqlite
